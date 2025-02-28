d47eb878e223be094c5aead0d453d4ff4949de35
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-27 21:47:04 -0300

    feat: add list

---
+++ src/main/java/com/github/acnaweb/study_apir/service/ProductService.java
@@ -0,0 +1,13 @@
+package com.github.acnaweb.study_apir.service;
+
+import java.util.List;
+
+import com.github.acnaweb.study_apir.modelo.Product;
+
+public class ProductService {
+
+    private List<Product> products;
+
+
+
+}
