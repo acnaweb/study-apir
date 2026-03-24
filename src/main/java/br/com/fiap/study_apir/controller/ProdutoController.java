a0e6e48e01d60f1396290b64b23bd5e5e8cd3d06
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-23 22:48:48 -0300

    feat: mockup find

--- src/main/java/br/com/fiap/study_apir/controller/ProdutoController.java
+++ src/main/java/br/com/fiap/study_apir/controller/ProdutoController.java
@@ -1,26 +1,39 @@
 package br.com.fiap.study_apir.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.fiap.study_apir.model.Produto;
+import br.com.fiap.study_apir.repository.RepositoryProdutoMockup;
+
 @RestController
-@RequestMapping("produtos")
+@RequestMapping("api/${api.version}/produtos")
 public class ProdutoController {
+
+    private RepositoryProdutoMockup mockup = new RepositoryProdutoMockup();
 
     @PostMapping
     public ResponseEntity<String> create() {
         return ResponseEntity.status(HttpStatus.CREATED).body("Produto criado");
     }
 
+    @GetMapping("/{id}")    
+    public ResponseEntity<Produto> findById(@PathVariable Long id) {        
+        return ResponseEntity.status(HttpStatus.OK).body(mockup.findById(id));
+    }
+
     @GetMapping    
-    public ResponseEntity<String> findById() {
-        return ResponseEntity.status(HttpStatus.OK).body("Maça");
+    public ResponseEntity<List<Produto>> findAll() {        
+        return ResponseEntity.status(HttpStatus.OK).body(mockup.findAll());
     }
 
     @PutMapping
